72a7deb678446f1b53136973c76d5f6c31b9ccdb
Cristiano <kristian0tj2019@gmail.com>
2023-03-06 14:49:48 -0300

    Filtering API information

--- script.js
+++ script.js
@@ -7,16 +7,27 @@ const fetchApi = (value) => {
     const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            // console.log(data);
             return data;
         });
 
     return result;
 }
 
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode'];
+
+const buildResult = (result) => {
+    const newObject = {};
+    keys.map((key) => document.getElementById(key))
+        .map((elem) => {
+            elem.checked && (newObject[elem.name] = result[elem.name]);
+        });
+    return newObject;
+}
+
 btnGo.addEventListener('click', async (event) => {
     event.preventDefault();
     const result = await fetchApi(characterId.value);
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
     image.src = `${result.image}`;
 });
